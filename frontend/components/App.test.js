98058968e67cdbef6f82b84216eb3f16815e8ddb
Robert Jank <robert.jank@yahoo.com>
2024-07-17 11:26:17 -0500

    last commit

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,44 +1,84 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import App from './App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+import txt from "../i18n/index.json";
 
-describe('Module 4 Project Tests', () => {
-  describe('English Language', () => {
+describe("Module 4 Project Tests", () => {
+  describe("English Language", () => {
     /*
       👉 TASK 1
 
       One test is done for you as an example.
     */
-    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
-      render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
-    })
-  })
-  describe('Spanish Language', () => {
-    /*
-      👉 TASK 3
-
-      This is done after making the UI multilingual.
-    */
-  })
-  describe('getEntriesByKeyPrefix', () => {
-    test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
-    })
-  })
-})
+    let texts = getEntriesByKeyPrefix(txt.en, "TEXT");
+    for (let [key, val] of texts) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+    let labels = getEntriesByKeyPrefix(txt.en, "LABEL");
+    for (let [key, val] of labels) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+    let placeholder = getEntriesByKeyPrefix(txt.en, "LPLACEHOLDER");
+    for (let [key, val] of placeholder) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+  });
+  describe("Spanish Language", () => {
+    let texts = getEntriesByKeyPrefix(txt.esp, "TEXT");
+    for (let [key, val] of texts) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+    let labels = getEntriesByKeyPrefix(txt.esp, "LABEL");
+    for (let [key, val] of labels) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+    let placeholder = getEntriesByKeyPrefix(txt.esp, "LPLACEHOLDER");
+    for (let [key, val] of placeholder) {
+      test(`${key} is visble`, () => {
+        render(<App />);
+        expect(screen.getByText(val)).toBeVisible();
+      });
+    }
+  });
+  describe("getEntriesByKeyPrefix", () => {
+    test("can extract the correct data", () => {
+      const obj = {
+        abc_1: "data_abc_1",
+        abc_2: "data_abc_2",
+        xyz_1: "data_xyz_1",
+        abc_3: "data_abc_3",
+      };
+      const expected = [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ];
+      const expected2 = [["xyz_1", "data_xyz_1"]];
+      expect(getEntriesByKeyPrefix(obj, "abc")).toEqual(expected);
+      expect(getEntriesByKeyPrefix(obj, "xyz")).toEqual(expected2);
+      expect(getEntriesByKeyPrefix(obj, "foo")).toEqual([]);
+    });
+  });
+});
 function getEntriesByKeyPrefix(obj, keyPrefix) {
+  return Object.entries(obj).filter(([key]) => key.split("_")[0] === keyPrefix);
+
   /*
     👉 TASK 4 part 1
 
